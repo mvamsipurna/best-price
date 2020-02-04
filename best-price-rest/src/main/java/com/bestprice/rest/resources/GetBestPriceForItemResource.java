b7a358368626ffd778eb169d9792ce2941c112da
Vamsi <vamsipurna.mummaneni@cerner.com>
2020-02-03 20:49:34 -0600

    configures guice

--- best-price-rest/src/main/java/com/bestprice/rest/resources/GetBestPriceForItemResource.java
+++ best-price-rest/src/main/java/com/bestprice/rest/resources/GetBestPriceForItemResource.java
@@ -1,20 +1,33 @@
 package com.bestprice.rest.resources;
 
+import com.bestprice.util.Verifier;
+import com.bestprice.util.VerifierException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Path("/fetch")
 public class GetBestPriceForItemResource {
+
+    private static final Logger LOGGER = Logger.getLogger(GetBestPriceForItemResource.class.getName());
 
     @GET
     @Path("/best-price/{item}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBestPriceForAnItem(@PathParam("item")  final String item) {
-        System.out.println("This api calculates best price");
+        try {
+            Verifier.verifyNotNull(item, "item should not be null");
+            Verifier.verifyNotEmpty(item, "not empty");
+        } catch (final VerifierException verifierException) {
+            LOGGER.log(Level.SEVERE,"generic error", verifierException);
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
         return null;
     }
 }
