94828f1e171c78b3b80de6b767299670ee193f74
Vamsi <vamsipurna.mummaneni@cerner.com>
2020-02-03 21:08:24 -0600

    configures redis for in memory db support

---
+++ best-price-jdbc/src/main/java/com/bestprice/util/RedisSource.java
@@ -0,0 +1,9 @@
+package com.bestprice.util;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisSource {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis();
+    }
+}
